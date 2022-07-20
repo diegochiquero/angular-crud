6aab563f717c1089dbe7b4bb9cfaf9ecd79ec605
diegochiquero <chiquerodiego@gmail.com>
2022-07-20 18:37:01 +0200

    feat: add server url

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+//import model
+import { Todo } from '../models/todo.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private serverUrl: string = 'https://jsonplaceholder.typicode.com';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  public getAllTodo(): Observable<Todo[]> {
+    let dataUrl: string = `${this.serverUrl}/todos`;
+    return this.httpClient.get<Todo[]>(dataUrl).pipe();
+  }
 }
