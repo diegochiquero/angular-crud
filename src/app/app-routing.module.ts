3999d3137cc5fddde81ba719d6ad16e35ac91957
diegochiquero <chiquerodiego@gmail.com>
2022-07-20 18:38:03 +0200

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//import Components
+import { HomeComponent } from './components/home/home.component';
+import { CreateTodoComponent } from './components/create-todo/create-todo.component';
+import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'todo/home', pathMatch: 'full' },
+  { path: 'todo/home', component: HomeComponent },
+  { path: 'todo/create', component: CreateTodoComponent },
+  { path: 'todo/edit', component: EditTodoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
