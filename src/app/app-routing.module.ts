cc5bc12c8f66c80aeb2a74a8a904d4fbae807d4f
diegochiquero <chiquerodiego@gmail.com>
2022-08-08 17:16:35 +0200

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 //import Components
-import { HomeComponent } from './components/home/home.component';
-import { CreateTodoComponent } from './components/create-todo/create-todo.component';
-import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
+import { HomeComponent } from './views/home/home.component';
+import { TodoCrudComponent} from './views/todo-crud/todo-crud.component';
+import { ReadTodoComponent } from './components/todo/read-todo/read-todo.component';
+import { CreateTodoComponent } from './components/todo/create-todo/create-todo.component';
+import { UpdateTodoComponent } from './components/todo/update-todo/update-todo.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'todo/home', pathMatch: 'full' },
   { path: 'todo/home', component: HomeComponent },
+  { path: 'todos', component: TodoCrudComponent},
+  { path: 'todo/read', component: ReadTodoComponent },
   { path: 'todo/create', component: CreateTodoComponent },
-  { path: 'todo/edit', component: EditTodoComponent },
+  { path: 'todo/update:id', component: UpdateTodoComponent },
 ];
 
 @NgModule({
