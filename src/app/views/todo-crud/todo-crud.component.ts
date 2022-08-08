4abb2f9909859d5778debec913336c9a53891e0e
diegochiquero <chiquerodiego@gmail.com>
2022-08-08 17:25:59 +0200

    feat: add views components

---
+++ src/app/views/todo-crud/todo-crud.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-todo-crud',
+  templateUrl: './todo-crud.component.html',
+  styleUrls: ['./todo-crud.component.css'],
+})
+export class TodoCrudComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+  navigateToCreateTodo(): void {
+    this.router.navigate(['todo/create']);
+  }
+}
