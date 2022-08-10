4bad717514f6b920fd94012825e126476a8a0190
diegochiquero <chiquerodiego@gmail.com>
2022-08-10 18:53:11 +0200

    feat: add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 //import components
 import { HeaderComponent } from './components/template/header/header.component';
@@ -30,7 +31,8 @@ import { UpdateTodoComponent } from './components/todo/update-todo/update-todo.c
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
