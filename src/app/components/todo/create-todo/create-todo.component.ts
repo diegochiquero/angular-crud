5e93c700aadbdc9e369055372df5ccb6d944371e
diegochiquero <chiquerodiego@gmail.com>
2022-08-22 16:10:01 +0200

    feat: add createSubmit function

--- src/app/components/todo/create-todo/create-todo.component.ts
+++ src/app/components/todo/create-todo/create-todo.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo.model';
+import { TodoService } from 'src/app/services/todo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-todo',
   templateUrl: './create-todo.component.html',
-  styleUrls: ['./create-todo.component.css']
+  styleUrls: ['./create-todo.component.css'],
 })
 export class CreateTodoComponent implements OnInit {
+  public todo: Todo = {} as Todo;
+  public errorMessage: string | null = null;
 
-  constructor() { }
+  constructor(private todoService: TodoService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public createSubmit() {
+    this.todoService.createTodo(this.todo).subscribe({
+      next: (data: Todo) => {
+        console.log(data);
+        this.router.navigate(['/todos']);
+      },
+      error: (error) => {
+        this.errorMessage = error;
+      },
+    });
   }
-
 }
