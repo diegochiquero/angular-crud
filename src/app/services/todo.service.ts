bd17f169ac555844819ed0fb3dbc3e6d9d371b42
diegochiquero <chiquerodiego@gmail.com>
2022-08-22 16:24:15 +0200

    feat: add create, update, delete todos

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { catchError, Observable, throwError, map } from 'rxjs';
 
 //import model
 import { Todo } from '../models/todo.model';
@@ -11,10 +15,66 @@ import { Todo } from '../models/todo.model';
 export class TodoService {
   private serverUrl: string = 'https://jsonplaceholder.typicode.com';
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
   constructor(private httpClient: HttpClient) {}
 
   public getAllTodo(): Observable<Todo[]> {
     let dataUrl: string = `${this.serverUrl}/todos`;
-    return this.httpClient.get<Todo[]>(dataUrl).pipe();
+    return this.httpClient
+      .get<Todo[]>(dataUrl)
+      .pipe(catchError(this.handleError));
+  }
+
+  public createTodo(todo: Todo): Observable<Todo> {
+    let dataUrl: string = `${this.serverUrl}/todos`;
+    return this.httpClient
+      .post<Todo>(
+        dataUrl,
+        JSON.stringify({
+          userId: 1,
+          title: 'holA',
+          completed: false,
+        }),
+        this.httpOptions
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  public readById(id: number): Observable<Todo> {
+    let dataUrl: string = `${this.serverUrl}/todos/${id}`;
+    return this.httpClient.get<Todo>(dataUrl).pipe(
+      map((obj) => obj),
+      catchError(this.handleError)
+    );
+  }
+  public updateTodo(todo: Todo, id: string): Observable<Todo> {
+    let dataUrl: string = `${this.serverUrl}/todos/${id}`;
+    return this.httpClient
+      .put<Todo>(dataUrl, todo)
+      .pipe(catchError(this.handleError));
+  }
+
+  public deleteTodo(id: string): Observable<{}> {
+    let dataUrl: string = `${this.serverUrl}/todos/${id}`;
+    return this.httpClient
+      .delete<{}>(dataUrl)
+      .pipe(catchError(this.handleError));
+  }
+
+  public handleError(error: HttpErrorResponse) {
+    let errorMessage: string = '';
+    if (error.error instanceof ErrorEvent) {
+      //Client error
+      errorMessage = `Error : ${error.error.message}`;
+    } else {
+      //Server error
+      errorMessage = `Status : ${error.status} \n Message ${error.message}`;
+    }
+    return throwError(errorMessage);
   }
 }
