add84cb00cb3aa52fda5251596e8d6919c00ae6f
diegochiquero <chiquerodiego@gmail.com>
2022-08-23 16:59:25 +0200

    feat: add delete route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { TodoCrudComponent} from './views/todo-crud/todo-crud.component';
 import { ReadTodoComponent } from './components/todo/read-todo/read-todo.component';
 import { CreateTodoComponent } from './components/todo/create-todo/create-todo.component';
 import { UpdateTodoComponent } from './components/todo/update-todo/update-todo.component';
+import { DeleteTodoComponent } from './components/todo/delete-todo/delete-todo.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'todo/home', pathMatch: 'full' },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'todo/read', component: ReadTodoComponent },
   { path: 'todo/create', component: CreateTodoComponent },
   { path: 'todo/update/:id', component: UpdateTodoComponent },
+  { path: 'todo/delete/:id', component: DeleteTodoComponent}
 ];
 
 @NgModule({
