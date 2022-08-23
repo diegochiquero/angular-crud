0bb98f53fb63c22a7b979ac97606505cc6e070b0
diegochiquero <chiquerodiego@gmail.com>
2022-08-23 17:00:49 +0200

    feat: import delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { TodoCrudComponent } from './views/todo-crud/todo-crud.component';
 import { ReadTodoComponent } from './components/todo/read-todo/read-todo.component';
 import { CreateTodoComponent } from './components/todo/create-todo/create-todo.component';
 import { UpdateTodoComponent } from './components/todo/update-todo/update-todo.component';
+import { DeleteTodoComponent } from './components/todo/delete-todo/delete-todo.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { UpdateTodoComponent } from './components/todo/update-todo/update-todo.c
     TodoCrudComponent,
     ReadTodoComponent,
     CreateTodoComponent,
-    UpdateTodoComponent
+    UpdateTodoComponent,
+    DeleteTodoComponent
   ],
   imports: [
     BrowserModule,
